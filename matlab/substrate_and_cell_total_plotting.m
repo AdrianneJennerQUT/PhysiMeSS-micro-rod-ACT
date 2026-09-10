273eb41cfbd1a817134be1bef9bb748e1e72efe8
Mason Lacy <mason.lacy@hdr.qut.edu.au>
2026-09-10 16:36:00 +1000

    Update substrate_and_cell_total_plotting.m
    Third measure of clustering now checks average distance from T cell to any rod cell

--- matlab/substrate_and_cell_total_plotting.m
+++ matlab/substrate_and_cell_total_plotting.m
@@ -103,35 +103,32 @@ for tcount = 1:timetotal
         closest_both = Inf;
         for j = [1:i-1 i+1:length(types)] % for each pair of agents (except agent i)
             curr_dist = norm(agent_pos(i,:)-agent_pos(j,:)); % current distance between both agents
-            if types(i) == types(j) % the pair are both T cells or both rods
-                if types(i) == 0 % both T cells
-                    %avgdist_cells(tcount) = avgdist_cells(tcount) + curr_dist; % increment distance
+            if types(i) == 0
+                if types(j) == 0 % both T cells
                     if curr_dist < closest_cells % current distance is closer than the prev closest
                         closest_cells = curr_dist;
                     end
-                else % both rod cells
-                    %avgdist_rods(tcount) = avgdist_rods(tcount) + curr_dist; % increment distance
-                    if curr_dist < closest_rods % current distance is closer than the prev closest
-                        closest_rods = curr_dist;
+                else % i is T cell, j is rod
+                    if curr_dist < closest_both % current distance is closer than the prev closest
+                        closest_both = curr_dist;
                     end
                 end
-            end
-            %avgdist_both(tcount) = avgdist_both(tcount) + curr_dist; % regardless of agent type, increment distance
-            if curr_dist < closest_both % current distance is closer than the prev closest
-                closest_both = curr_dist;
+            elseif types(j) == 1 % both rods
+                if curr_dist < closest_rods % current distance is closer than the prev closest
+                    closest_rods = curr_dist;
+                end
             end
         end
         if types(i) == 0
             avgdist_cells(tcount) = avgdist_cells(tcount) + closest_cells; % add closest distance to current timestep
-        end
-        if types(i) == 1
+            avgdist_both(tcount) = avgdist_both(tcount) + closest_both;
+        else
             avgdist_rods(tcount) = avgdist_rods(tcount) + closest_rods;
         end
-        avgdist_both(tcount) = avgdist_both(tcount) + closest_both;
     end
     avgdist_cells(tcount) = avgdist_cells(tcount)/sum(types==0); % average the results
     avgdist_rods(tcount) = avgdist_rods(tcount)/sum(types==1);
-    avgdist_both(tcount) = avgdist_both(tcount)/length(types);
+    avgdist_both(tcount) = avgdist_both(tcount)/sum(types==0);
 
 
     %plotting contour plot of substrate
@@ -224,13 +221,24 @@ set(gca,'FontSize',18)
 
 %% testing rod position and rotation
 
-agent_pos = MCDS.discrete_cells.state.position(:,1:2);
+num_cells = 500;
+num_rods = 100; 
 
-cell_pos = agent_pos(types==0,:);
+types = zeros(num_cells+num_rods,1); types(1:num_rods) = 1;
 
-rod_pos = agent_pos(types==1,:);
-rod_rot = MCDS.discrete_cells.state.orientation(types==1,:);
+cell_mean = 300; cell_var = 1;
+rod_mean = 100; rod_var = 100;
 
+cell_pos = cell_mean + cell_var*randn(num_cells,2);
+rod_pos = rod_mean + rod_var*randn(num_rods,2);
+agent_pos = [rod_pos; cell_pos];
+
+%agent_pos = MCDS.discrete_cells.state.position(:,1:2);
+%cell_pos = agent_pos(types==0,:);
+%rod_pos = agent_pos(types==1,:);
+%rod_rot = MCDS.discrete_cells.state.orientation(types==1,:);
+
+figure
 scatter(cell_pos(:,1), cell_pos(:,2))
 hold on
 scatter(rod_pos(:,1), rod_pos(:,2), color="red")
@@ -244,32 +252,29 @@ for i = 1:length(types)
     closest_both = Inf;
     for j = [1:i-1 i+1:length(types)] % for each pair of agents (except agent i)
         curr_dist = norm(agent_pos(i,:)-agent_pos(j,:)); % current distance between both agents
-        if types(i) == types(j) % the pair are both T cells or both rods
-            if types(i) == 0 % both T cells
-                %avgdist_cells(tcount) = avgdist_cells(tcount) + curr_dist; % increment distance
+        if types(i) == 0
+            if types(j) == 0 % both T cells
                 if curr_dist < closest_cells % current distance is closer than the prev closest
                     closest_cells = curr_dist;
                 end
-            else % both rod cells
-                %avgdist_rods(tcount) = avgdist_rods(tcount) + curr_dist; % increment distance
-                if curr_dist < closest_rods % current distance is closer than the prev closest
-                    closest_rods = curr_dist;
+            else % i is T cell, j is rod
+                if curr_dist < closest_both % current distance is closer than the prev closest
+                    closest_both = curr_dist;
                 end
             end
-        end
-        %avgdist_both(tcount) = avgdist_both(tcount) + curr_dist; % regardless of agent type, increment distance
-        if curr_dist < closest_both % current distance is closer than the prev closest
-            closest_both = curr_dist;
+        elseif types(j) == 1 % both rods
+            if curr_dist < closest_rods % current distance is closer than the prev closest
+                closest_rods = curr_dist;
+            end
         end
     end
     if types(i) == 0
         avgdist_cells = avgdist_cells + closest_cells; % add closest distance to current timestep
-    end
-    if types(i) == 1
+        avgdist_both = avgdist_both + closest_both;
+    else
         avgdist_rods = avgdist_rods + closest_rods;
     end
-    avgdist_both = avgdist_both + closest_both;
 end
 avgdist_cells = avgdist_cells/sum(types==0); % average the results
 avgdist_rods = avgdist_rods/sum(types==1);
-avgdist_both = avgdist_both/length(types);
+avgdist_both = avgdist_both/sum(types==0);
